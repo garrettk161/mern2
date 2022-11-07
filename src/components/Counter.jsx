2094714ec848909a388b2944d351241c3e7491d5
garrettk161 <garrettk161@gmail.com>
2022-11-07 12:29:34 -0700

    g

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const Counter = () => {
-    let countState = 77
+    const [countState, setCountState] = useState(77)
+
+    function incCount() {
+        setCountState(countState +1)
+    }
     return (
         <div>
             <div>{countState}</div>
-            <button>+1</button>
+            <button onClick={incCount}>+1</button>
         </div>
     )
 }
